cc8100fc69be4bf4ff329df60e76e9a0b3f3de58
dheerajmekala <dheeraj7596.dm@gmail.com>
2024-05-04 03:46:44 +0530

    updated combine

--- training/data/combine.py
+++ training/data/combine.py
@@ -1,13 +1,10 @@
 import json
+import sys
 
 if __name__ == "__main__":
-    f = open("/Users/dheerajmekala/Work/lost-in-the-middle/training/data/gsm8k_1000_distractors.jsonl", "r")
+    gsm8k_list = json.load(open(sys.argv[1], "r"))
+    longest_list = json.load(open(sys.argv[2], "r"))
+    final_list = longest_list + gsm8k_list
 
-    longest_list = json.load(open("/Users/dheerajmekala/Work/lost-in-the-middle/training/data/longest_5667.json", "r"))
-    lines = f.readlines()
-    for l in lines:
-        temp = json.loads(l.strip())
-        longest_list.append(temp)
-
-    json.dump(longest_list,
-              open("/Users/dheerajmekala/Work/lost-in-the-middle/training/data/longest_distractods.json", "w"))
+    json.dump(final_list,
+              open(sys.argv[3], "w"))
